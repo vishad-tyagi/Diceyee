90ea59d7cfd8742393d5628d88935979611383c5
vishad-tyagi <78733360+vishad-tyagi@users.noreply.github.com>
2022-06-28 01:43:09 +0530

    Initial Commit

---
+++ Diceyee/DiceyeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceyeeApp.swift
+//  Diceyee
+//
+//  Created by Vishad Tyagi on 28/06/22.
+//
+
+import SwiftUI
+
+@main
+struct DiceyeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
